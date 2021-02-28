a3ac28af6a297833874f53303e132865aa0c8d3b
Samin1615088 <samin.shafkat@gmail.com>
2021-03-01 00:27:26 +0600

    header is added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="shop">Shop</a></li>
+                    <li><a href="order">Order Review</a></li>
+                    <li><a href="inventory">Manage Inventory here</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
